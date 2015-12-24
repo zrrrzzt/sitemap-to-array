245fdb9d8449337378572e937f5859cc7c82b111
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-25 00:44:03 +0100

    Implements new interface

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ var streamifier = require('streamifier')
 var streamify = require('streamify')
 var convertStream = require('./lib/convertSitemapData')
 
-function sitemapToArray (sitemap, callback) {
+function sitemapToArray (sitemap, options, callback) {
   if (!sitemap) {
     return callback(new Error('Missing required input: sitemap.'), null)
   }
 
-  var list = []
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+
+  var returnOnComplete = options.returnOnComplete || false
+  var list = returnOnComplete ? [] : require('stream').PassThrough()
   var stream = false
   var err = null
 
@@ -25,19 +31,30 @@ function sitemapToArray (sitemap, callback) {
   }
 
   convertStream.on('data', function (data) {
-    list.push(JSON.parse(data))
+    list.push(returnOnComplete ? JSON.parse(data.toString()) : data)
   })
 
   convertStream.on('end', function () {
-    return callback(err, list)
+    if (returnOnComplete) {
+      return callback(err, list)
+    } else {
+      list.push(null)
+    }
   })
 
   convertStream.on('error', function (error) {
     err = error
+    if (!returnOnComplete) {
+      list.emit('error', error)
+    }
   })
 
   stream
     .pipe(convertStream)
+
+  if (!returnOnComplete) {
+    return callback(list)
+  }
 }
 
 module.exports = sitemapToArray
